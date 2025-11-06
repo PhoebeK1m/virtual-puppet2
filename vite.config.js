2e8cd48a13af01104cc77f97d469bd2a9026ce13
Ph0ebeK1m <phoebekim12006@gmail.com>
2025-11-06 14:28:59 -0600

    config file fixe

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
-import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: '/', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'live.html'),
+      },
+    },
+  },
 });
+
