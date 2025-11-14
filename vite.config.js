47abb413a624450ecb9c0a307278fcec8e92492f
Ph0ebeK1m <phoebekim12006@gmail.com>
2025-11-14 11:16:07 -0600

    congif + directory

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'live.html'),
+        about: resolve(__dirname, 'resume.html'),
       },
     },
   },
